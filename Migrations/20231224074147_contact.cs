0ff7df56c6e9cf759626fb064ba6f1b0ee814fa0
thien1 <thiencualo1202@gmail.com>
2023-12-24 16:11:51 +0700

    dak

---
+++ Migrations/20231224074147_contact.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace startup.Migrations
+{
+    public partial class contact : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "contacts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateSend = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Message = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_contacts", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "contacts");
+        }
+    }
+}
---
+++ Models/products/productmodel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace startup.Models.products
+{
+    public class ProductModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+       public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+       public int Quantity { get; set; }
+
+        public string Slug { get; set; }
+
+        public string source { get; set; }
+
+        public string category { get; set; }
+
+        public DateTime hsd { get; set; }
+
+        public string Image { get; set; }
+
+        public DateTime DateCreated { get; set; }
+    }
+
+}
